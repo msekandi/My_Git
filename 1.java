a039b5d9ce4a29800dd5e5c5865765b96ac6e7eb
Ubuntu <ubuntu@ip-172-31-92-52.ec2.internal>
2023-07-21 02:22:09 +0000

    testing from 52

--- 1.java
+++ 1.java
@@ -3,5 +3,10 @@ This is for Testing conflicts.
 This is from the feature branch.
 This is from the defect branch.
 
+
 Testing rebase operation on defect.
+
+Testing rebase operation on feature branch.
+
+
 
